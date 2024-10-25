5a89c3b9a543053065ae7b0ad8397fd7a92f4fa9
csenger7 <lengyelcs@kkszki.hu>
2024-10-25 11:59:14 +0200

    Program is ready

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace Doga_2
     {
         static void Main(string[] args)
         {
+            int pozitivOsszeg = 0;
+            int negativOsszeg = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Kérem, adjon meg egy egész számot ({i + 1}/10): ");
+                int szam = int.Parse(Console.ReadLine());
+
+                if (szam > 0)
+                {
+                    pozitivOsszeg += szam; 
+                }
+                else if (szam < 0)
+                {
+                    negativOsszeg += szam; 
+                }
+                
+            }
+
+            Console.WriteLine($"A pozitív számok összege: {pozitivOsszeg}");
+            Console.WriteLine($"A negatív számok összege: {negativOsszeg}");
         }
     }
 }
